3c664db0a548d582b87f6b1f62f0b9c71084747c
segfault <s9gf4ult@gmail.com>
2013-11-03 21:42:49 +0600

    offcom uniq name

--- app/models/entities/offer_component.rb
+++ app/models/entities/offer_component.rb
@@ -1,5 +1,6 @@
 class OfferComponent < ActiveRecord::Base
   attr_accessible :content, :name
+  validates :name, :presence => true, :uniqueness => true
 
   sortable :by => ["name ASC", "created_at DESC", "updated_at DESC"], :default => "created_at DESC"
   has_paper_trail
